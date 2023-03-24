edfabc123db054090debb1c4e3d8b12ce54cc0de
paulopraddo <paulopraddo011@gmail.com>
2023-03-24 13:56:11 -0300

    ultilizando-o-express

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-const http = require('http'); 
+import http from "http"
+
+import app from "./src/app.js"
+
 const port = 3000;
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros' : 'Pagina de Livros',
-    '/autores' : 'Pagina de Autores',
-    '/admins': 'Borrachinha Derruba Servidor'
-}
-
-
-const server = http.createServer((req,res) => {
-    res.writeHead(200, {'Content-type': 'text/plain'})
-    res.end(rotas[req.url])
-
-})
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Escutando em http://localhost:${port}`)
 })
