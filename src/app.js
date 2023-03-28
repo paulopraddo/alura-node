2fef0ebf8da23a7ab721d0ee8e311459bc3ab202
paulopraddo <paulopraddo011@gmail.com>
2023-03-28 14:17:51 -0300

    adicionando-rotas

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const livros = [
     {id:2, "titulo": "O Hobiit"}
 ]
 
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
+
 app.use(express.json())   
 
 app.get('/',(req,res) => {
@@ -17,6 +21,10 @@ app.get('/livros',(req,res) => {
     res.status(200).json(livros)
 })
 
+app.get('/livros/:id', (req,res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
 
 app.post('/livros',(req,res) => {
     livros.push(req.body);
@@ -29,9 +37,11 @@ app.put('/livros/:id', (req,res) => {
     res.json(livros);
 })
 
-function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id == id)
-}
-
+app.delete('/livros/:id', (req,res) => {
+    let {id} = req.params
+    let index = buscaLivro(id);
+    livros.splice(index,1)
+    res.send(`Livro ${id} removido com sucesso`);
+})
 
 export default app
